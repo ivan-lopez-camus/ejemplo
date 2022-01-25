b967f63537cf06a3b6d3f050280163fd5e092351
Ivan López <ivan.lopez1@unmsm.edu.pe>
2022-01-25 17:19:02 -0500

    componentes basicos y rutas

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">Ejemplo proyecyo en Vue</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Proyecto para obtener las cotizaciones de las cryptomonedas mas
+      importantes a traves de la API REST de Coincap.
+      <br />Este proyecto utiliza la version 2 de Vue
+    </p>
+
+    <router-link
+      to="/"
+      class="mt-5 text-xl text-green-600 hover:underline"
+    >Volver a la pagina de Inicio</router-link>
+  </div>
+</template>
