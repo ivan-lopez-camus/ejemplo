3f568b8b3d4bc26175f96d223bb7ac8e2ca37de8
Ivan López <ivan.lopez1@unmsm.edu.pe>
2022-01-25 22:06:34 -0500

    consumo de una api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,27 @@
 <template>
     <div>
-        <px-assets-table />     
+        <px-assets-table :assets="assets" />     
     </div>
 </template>
 
 <script>
 import PxAssetsTable from '@/components/PxAssetsTable';
+import api from '@/api';
 
 export default {
    name: 'Home',
-   components: {PxAssetsTable}
+   components: {PxAssetsTable},
+
+   data (){
+       return {
+           assets:[]
+       }
+   },
+
+   created () {
+       api.getAssets()
+         .then(assets => (this.assets = assets))
+   }
 }
 </script>
 
